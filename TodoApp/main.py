43d550bc5c60ba42a4897706f8c3257d1b20cd40
unknown <vitthalgoel7@gmail.com>
2025-03-03 10:54:08 +0530

    main.py updated

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -4,19 +4,25 @@ from .models import Base
 from .database import engine
 from .routers import auth, todos, admin, users
 from fastapi.templating import Jinja2Templates
+import os
 
 app = FastAPI()
 Base.metadata.create_all(bind=engine)
 
-# Mount static directories
-app.mount("/static", StaticFiles(directory="TodoApp/static"), name="static")
-app.mount("/images", StaticFiles(directory="TodoApp/static/images"), name="images")
+# Get the current directory
+current_dir = os.path.dirname(os.path.abspath(__file__))
 
-templates = Jinja2Templates(directory="TodoApp/templates")
+# Mount static directories with correct paths
+app.mount("/static", StaticFiles(directory=os.path.join(current_dir, "static")), name="static")
+app.mount("/images", StaticFiles(directory=os.path.join(current_dir, "static/images")), name="images")
+
+# Set templates directory with correct path
+templates = Jinja2Templates(directory=os.path.join(current_dir, "templates"))
 
 @app.get("/")
 def test(request: Request):
-    return templates.TemplateResponse("home.html",{"request":request})
+    return templates.TemplateResponse("home.html", {"request": request})
+
 @app.get("/healthy")
 def healthcheck():
     return {"status": "healthy"}
